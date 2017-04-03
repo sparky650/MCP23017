e1f828090903ee531c8d3c53c7185daed2e3f038
Keegan Morrow <meserv@gmail.com>
2017-04-03 18:13:07 -0400

    Changed readSetWritePin to readModifyWritePin

--- src/MCP23017.h
+++ src/MCP23017.h
@@ -93,13 +93,16 @@ public:
 	void setInputPolarity(MCP23017_Port_t, bool);
 	void setInputPolarity(uint8_t, bool);
 
+	uint8_t getInterrupt();
+	uint16_t getInterruptCapture();
+	uint8_t getInterruptCapture(MCP23017_Port_t);
 	void setInterrupt(uint8_t, bool);
 	void setInterrupt(uint16_t);
 	void interruptMirror(bool);
 	void setIntPinMode(MCP23017_interruptPinMode_t);
 
 private:
-	template<MCP23017_RegisterGeneric_t reg> void readSetWritePin(uint8_t, bool);
+	template<MCP23017_RegisterGeneric_t reg> void readModifyWritePin(uint8_t, bool);
 	inline MCP23017_Port_t pinToPort(uint8_t pin) {return (pin < 8) ? A : B;}
 	inline uint8_t pinToBit(uint8_t pin) {return pin % 8;}
 	inline uint8_t pinToMask(uint8_t pin) {return 1 << (pin % 8);}
