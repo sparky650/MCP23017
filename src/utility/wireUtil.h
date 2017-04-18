32aff8d81b85f36a271dae2551aee6780300dff0
Keegan Morrow <meserv@gmail.com>
2017-04-17 21:46:04 -0400

    Bump to version 1.1.2

--- src/utility/wireUtil.h
+++ src/utility/wireUtil.h
@@ -1,7 +1,7 @@
 /**
  * @file	wireUtil.h
  * @author	Keegan Morrow
- * @version	1.0.0
+ * @version	1.1.2
  * @brief Utility base class for reading and writing registers on i2c devices
  *
  */
@@ -16,8 +16,9 @@
  * @brief Utility base class for reading and writing registers on i2c devices
  *
  * @tparam REGTYPE An initialized enum type that lists the valid registers for the device
+ * @tparam DATATYPE = uint8_t Data type (register size) supports uint8_t, uint16_t, uint32_t
  */
-template <typename REGTYPE>
+template <typename REGTYPE, typename DATATYPE = uint8_t>
 class wireUtil
 {
 public:
@@ -39,7 +40,7 @@ public:
 
 	/**
 	 * @brief Safe method to read the state of the timeout flag
-	 * 
+	 *
 	 * @return State of the timeout flag
 	 */
 	inline bool getTimeoutFlag() {return timeoutFlag;}
@@ -50,10 +51,10 @@ public:
 	virtual void begin(uint8_t, uint8_t, uint8_t);
 #endif // ARDUINO_ARCH_ESP8266
 
-	bool writeRegister(REGTYPE, uint8_t);
-	bool writeRegisters(REGTYPE, uint8_t *, uint8_t);
-	uint8_t readRegister(REGTYPE);
-	bool readRegisters(REGTYPE, uint8_t *, uint8_t);
+	bool writeRegister(REGTYPE, DATATYPE);
+	bool writeRegisters(REGTYPE, DATATYPE *, uint8_t);
+	DATATYPE readRegister(REGTYPE);
+	bool readRegisters(REGTYPE, DATATYPE *, uint8_t);
 	bool setRegisterBit(REGTYPE, uint8_t, bool);
 
 protected:
@@ -62,6 +63,8 @@ protected:
 private:
 	void (*timeOutHandler)(void);
 	void (*errorHandler)(uint8_t);
+	void writeAsBytes(DATATYPE);
+	DATATYPE readAsBytes();
 };
 
 /*
@@ -74,8 +77,8 @@ private:
  *
  * @param address  Address of the chip
  */
-template <typename REGTYPE>
-void wireUtil<REGTYPE>::begin(uint8_t address)
+template <typename REGTYPE, typename DATATYPE>
+void wireUtil<REGTYPE, DATATYPE>::begin(uint8_t address)
 {
 	this->address = address;
 	Wire.begin();
@@ -90,8 +93,8 @@ void wireUtil<REGTYPE>::begin(uint8_t address)
  * @param SDApin Pin number for the SDA signal
  * @param SCLpin Pin number for the SCL signal
  */
-template <typename REGTYPE>
-void wireUtil<REGTYPE>::begin(uint8_t address, uint8_t SDApin, uint8_t SCLpin)
+template <typename REGTYPE, typename DATATYPE>
+void wireUtil<REGTYPE, DATATYPE>::begin(uint8_t address, uint8_t SDApin, uint8_t SCLpin)
 {
 	this->address = address;
 	Wire.begin(SDApin, SCLpin);
@@ -105,8 +108,8 @@ void wireUtil<REGTYPE>::begin(uint8_t address, uint8_t SDApin, uint8_t SCLpin)
  * @param data Data to be written to the device
  * @return true on success, false if NACK
  */
-template <typename REGTYPE>
-bool wireUtil<REGTYPE>::writeRegister(REGTYPE reg, uint8_t data)
+template <typename REGTYPE, typename DATATYPE>
+bool wireUtil<REGTYPE, DATATYPE>::writeRegister(REGTYPE reg, DATATYPE data)
 {
 	return writeRegisters(reg, &data, 1);
 }
@@ -119,14 +122,15 @@ bool wireUtil<REGTYPE>::writeRegister(REGTYPE reg, uint8_t data)
  * @param len Number of bytes in the array
  * @return true on success, false if NACK
  */
-template <typename REGTYPE>
-bool wireUtil<REGTYPE>::writeRegisters(REGTYPE reg, uint8_t *buffer, uint8_t len)
+template <typename REGTYPE, typename DATATYPE>
+bool wireUtil<REGTYPE, DATATYPE>::writeRegisters(REGTYPE reg, DATATYPE *buffer, uint8_t len)
 {
 	Wire.beginTransmission(address);
 	Wire.write(reg);
-	for (uint8_t x = 0; x < len; x++)
+	for (uint8_t i = 0; i < len; i++)
 	{
-		Wire.write(buffer[x]);
+		if (sizeof(DATATYPE) == 1) { Wire.write(buffer[i]); }
+		else { writeAsBytes(buffer[i]); }
 	}
 
 	uint8_t status;
@@ -146,18 +150,18 @@ bool wireUtil<REGTYPE>::writeRegisters(REGTYPE reg, uint8_t *buffer, uint8_t len
  * @param reg Register address (from a device specific enum)
  * @return Data from the device register, 0 if there is a timeout
  */
-template <typename REGTYPE>
-uint8_t wireUtil<REGTYPE>::readRegister(REGTYPE reg)
+template <typename REGTYPE, typename DATATYPE>
+DATATYPE wireUtil<REGTYPE, DATATYPE>::readRegister(REGTYPE reg)
 {
 	unsigned long abortTime;
 	Wire.beginTransmission(address);
 	Wire.write((uint8_t)reg);
 	Wire.endTransmission(false);
-	Wire.requestFrom(address, (uint8_t) 1);
+	Wire.requestFrom(address, (uint8_t)sizeof(DATATYPE));
 
 	timeoutFlag = false;
 	abortTime = millis() + timeoutTime;
-	while (!Wire.available())
+	while (Wire.available() < (int8_t)sizeof(DATATYPE))
 	{
 		if (abortTime < millis())
 		{
@@ -166,8 +170,8 @@ uint8_t wireUtil<REGTYPE>::readRegister(REGTYPE reg)
 			return 0;
 		}
 	}
-
-	return Wire.read();
+	if (sizeof(DATATYPE) == 1) { return Wire.read(); }
+	else { return readAsBytes(); }
 }
 
 /**
@@ -178,51 +182,118 @@ uint8_t wireUtil<REGTYPE>::readRegister(REGTYPE reg)
  * @param len Number of bytes to read
  * @return true on success, false on timeout
  */
-template <typename REGTYPE>
-bool wireUtil<REGTYPE>::readRegisters(REGTYPE reg, uint8_t *buffer, uint8_t len)
+template <typename REGTYPE, typename DATATYPE>
+bool wireUtil<REGTYPE, DATATYPE>::readRegisters(REGTYPE reg, DATATYPE *buffer, uint8_t len)
 {
 	Wire.beginTransmission(address);
 	Wire.write((uint8_t)reg);
 	Wire.endTransmission(false);
 
-	Wire.requestFrom(address, len);
-
 	timeoutFlag = false;
 	unsigned long abortTime = millis() + timeoutTime;
-	while (Wire.available() < len)
+	size_t bufferSize = len * sizeof(DATATYPE);
+	memset(buffer, 0, bufferSize);
+
+	Wire.requestFrom(address, (uint8_t) bufferSize);
+
+	while (Wire.available() < bufferSize)
 	{
 		if (abortTime < millis())
 		{
 			timeoutFlag = true;
-			memset(buffer, 0, len);
 			if (timeOutHandler != NULL) { (*timeOutHandler)(); }
 			return false; // timeout condition, return false
 		}
 	}
 
-	for (uint8_t x = 0 ; x < len ; x++)
+	for (uint8_t i = 0 ; i < len ; i++)
 	{
-		buffer[x] = Wire.read();
+		if (sizeof(DATATYPE) == 1) { buffer[i] = Wire.read(); }
+		else { buffer[i] = readAsBytes(); }
 	}
+
 	return true;
 }
 
 /**
  * @brief Read modify write a bit on a register
- * 
+ *
  * @param reg register to modify
  * @param bit index of the bit to set
  * @param state state of the bit to set
  * @return true on success
  */
-template <typename REGTYPE>
-bool wireUtil<REGTYPE>::setRegisterBit(REGTYPE reg, uint8_t bit, bool state)
+template <typename REGTYPE, typename DATATYPE>
+bool wireUtil<REGTYPE, DATATYPE>::setRegisterBit(REGTYPE reg, uint8_t bit, bool state)
 {
-	uint8_t tempReg;
+	DATATYPE tempReg;
 	tempReg = readRegister(reg);
-	if (state) { tempReg |= (uint8_t)(1 << bit); }
-	else { tempReg &= ~(uint8_t)(1 << bit); }
+	if (state) { tempReg |= (DATATYPE)(1 << bit); }
+	else { tempReg &= ~(DATATYPE)(1 << bit); }
 	return writeRegister(reg, tempReg);
+}
+
+/**
+ * @brief Assembles and writes a big endian packet
+ *
+ * @param d Unit of data to be written
+ */
+template <typename REGTYPE, typename DATATYPE>
+void wireUtil<REGTYPE, DATATYPE>::writeAsBytes(DATATYPE d)
+{
+	uint8_t buffer[sizeof(DATATYPE)];
+	switch (sizeof(DATATYPE))
+	{
+	case 4:
+		buffer[3] = d & 0xFF;
+		d = d >> 8;
+		buffer[2] = d & 0xFF;
+		d = d >> 8;
+		buffer[1] = d & 0xFF;
+		d = d >> 8;
+		buffer[0] = d & 0xFF;
+		break;
+	case 2:
+		buffer[1] = d & 0xFF;
+		buffer[0] = (d >> 8) & 0xFF;
+		break;
+	case 1:
+		buffer[0] = d;
+		break;
+	default:
+		memset(buffer, 0x00, sizeof(DATATYPE));
+	}
+	Wire.write(buffer, sizeof(DATATYPE));
+}
+
+/**
+ * @brief Receives a big endian packet and converts to a data unit
+ *
+ * @return The re-assembled data unit
+ */
+template <typename REGTYPE, typename DATATYPE>
+DATATYPE wireUtil<REGTYPE, DATATYPE>::readAsBytes()
+{
+	DATATYPE d;
+	switch (sizeof(DATATYPE))
+	{
+	case 4:
+		d = (uint32_t)Wire.read() << 24;
+		d |= (uint32_t)Wire.read() << 16;
+		d |= (uint32_t)Wire.read() << 8;
+		d |= (uint32_t)Wire.read();
+		break;
+	case 2:
+		d = (uint16_t)Wire.read() << 8;
+		d |= (uint16_t)Wire.read();
+		break;
+	case 1:
+		d = (uint8_t)Wire.read();
+		break;
+	default:
+		d = 0;
+	}
+	return d;
 }
 
 #endif // __wireUtil_h_
